51967ccae7a0a5bed7ef3c585f4bae2d66efd722
Robert Soule <robert.soule@gmail.com>
2017-04-25 14:52:23 -0700

    MRI

--- examples/baby_int.p4app/send.py
+++ examples/baby_int.p4app/send.py
@@ -5,16 +5,23 @@ import random
 import struct
 
 from scapy.all import sendp, send
-from scapy.all import Packet
+from scapy.all import Packet, IPOption
 from scapy.all import Ether, IP, UDP
-from scapy.all import IntField, FieldListField
+from scapy.all import IntField, FieldListField, FieldLenField, ShortField
+from scapy.layers.inet import _IPOption_HDR
 
-
-class MRI(Packet):
+class IPOption_MRI(IPOption):
     name = "MRI"
-    fields_desc = [ IntField("count", 0),
-                   FieldListField("swids", [], IntField("", 0),
-                                    length_from=lambda pkt:pkt.count) ]
+    option = 31
+    fields_desc = [ _IPOption_HDR,
+                    FieldLenField("length", None, fmt="B",
+                                  length_of="swids",
+                                  adjust=lambda pkt,l:l+4),
+                    ShortField("count", 0),
+                    FieldListField("swids",
+                                   [],
+                                   IntField("", 0),
+                                   length_from=lambda pkt:pkt.count) ]
     # def post_build(self, p, pay):
     #     if self.count is None:
     #         self.count = len(self.swids)
@@ -24,19 +31,10 @@ def main():
     
     addr = socket.gethostbyname(sys.argv[1])
 
-    pkt = Ether() / IP(dst=addr)/ UDP(dport=8000) / MRI(count=2, swids=[3,4]) / "hello"
+    pkt = Ether() / IP(dst=addr, options = IPOption_MRI(count=2, swids=[3,4])) / UDP(dport=8000) / "hello"
     pkt.show()
     sendp(pkt, iface="en0")
     
-    # values = (2, 5, 7)
-    # packer = struct.Struct('> I I I')
-    # packed_data = packer.pack(*values)
-
-                                   
-    # srv_addr = (sys.argv[1], int(sys.argv[2]))
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 99)
-    # sock.sendto(packed_data, srv_addr)
 
 if __name__ == '__main__':
     main()
