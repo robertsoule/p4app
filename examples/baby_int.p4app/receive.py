c0bcbba9a2ba7c5b440239f0db84a6f8e3c3bea0
Robert Soule <robert.soule@gmail.com>
2017-04-25 14:29:53 -0700

    send/receive packets with baby int header

--- examples/baby_int.p4app/receive.py
+++ examples/baby_int.p4app/receive.py
@@ -1,10 +1,10 @@
+import sys
+import struct
 
 from scapy.all import sniff, sendp
 from scapy.all import Packet
 from scapy.all import ShortField, IntField, LongField, BitField
-
-import sys
-import struct
+from scapy.all import IP, UDP, Raw
 
 
 # import socket, sys
@@ -35,13 +35,34 @@ def handle_pkt(pkt):
     print "got a packet"
     #pkt = str(pkt)
     #print pkt
+
+    pkt.show2()
+    sys.stdout.flush()
+    data = pkt[Raw].load
+    print "------------"
+    print str(data)
+    # count = struct.unpack(">I", data[0:4])[0]
+    count = struct.unpack(">I", data[0:4])[0]
+    print "count = %d" % count
+    for i in range(1,count+1):
+        lhs = 4 * i
+        rhs = lhs+4
+        # swid = struct.unpack(">I", data[lhs:rhs])[0]
+        swid = struct.unpack(">I", data[lhs:rhs])[0]
+        print "    swid = %d" % swid
+    lhs = 4 * count + 4
+    load = data[lhs:]
+    print "msg = %s" % load
     sys.stdout.flush()
 
+
+    
 
 def main():
-    print "sniffing on h2-eth0"
+    iface = sys.argv[1] # "h2-eth0"
+    print "sniffing on %s" % iface
     sys.stdout.flush()
-    sniff(iface = "h2-eth0",
+    sniff(filter="udp and port 8000", iface = iface,
           prn = lambda x: handle_pkt(x))
 
 if __name__ == '__main__':
--- examples/baby_int.p4app/send.py
+++ examples/baby_int.p4app/send.py
@@ -4,20 +4,39 @@ import socket
 import random
 import struct
 
+from scapy.all import sendp, send
+from scapy.all import Packet
+from scapy.all import Ether, IP, UDP
+from scapy.all import IntField, FieldListField
 
+
+class MRI(Packet):
+    name = "MRI"
+    fields_desc = [ IntField("count", 0),
+                   FieldListField("swids", [], IntField("", 0),
+                                    length_from=lambda pkt:pkt.count) ]
+    # def post_build(self, p, pay):
+    #     if self.count is None:
+    #         self.count = len(self.swids)
+            
+    
 def main():
-
+    
     addr = socket.gethostbyname(sys.argv[1])
 
-    values = (2, 1, 2)
-    packer = struct.Struct('> I I I')
-    packed_data = packer.pack(*values)
+    pkt = Ether() / IP(dst=addr)/ UDP(dport=8000) / MRI(count=2, swids=[3,4]) / "hello"
+    pkt.show()
+    sendp(pkt, iface="en0")
+    
+    # values = (2, 5, 7)
+    # packer = struct.Struct('> I I I')
+    # packed_data = packer.pack(*values)
 
                                    
-    srv_addr = (sys.argv[1], int(sys.argv[2]))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 99)
-    sock.sendto(packed_data + "hello", srv_addr)
+    # srv_addr = (sys.argv[1], int(sys.argv[2]))
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 99)
+    # sock.sendto(packed_data, srv_addr)
 
 if __name__ == '__main__':
     main()
